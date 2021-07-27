b75fde512ff4b0a7968924fb383314f3b1be40ea
RodrigoPlutino <rodrigoplutino@gmail.com>
2021-07-27 00:43:57 -0300

    pequenos cambios

--- my-app/src/components/NavBar.js
+++ my-app/src/components/NavBar.js
@@ -25,7 +25,7 @@ const acumulador = order.reduce((acu,element)=>acu + element.cantidad,0)
   return (
     <header>
       <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="#"><img className="nav-img" src="https://image.flaticon.com/icons/png/128/1532/1532495.png"></img></a>
+        <a class="navbar-brand" href="/"><img className="nav-img" src="https://image.flaticon.com/icons/png/128/1532/1532495.png"></img></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
           <span class="navbar-toggler-icon"></span>
         </button>
